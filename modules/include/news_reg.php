1db077cc4ee3d6fc5def9fd579e13fc7b19dc54d
Elizabeth González <elizabethpfaeskillsup@gmail.com>
2022-09-29 11:48:51 +0100

    Limpiar datos

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -1,33 +1,49 @@
 <?php
-require"../require/config.php";
+require"../requiere/config.php";     
+
+
 
 //define y inicializa las variables que se van a usar del formulario.
 $name = $email = $phone = $street = $city = $state = $zip = $other = $news = $newscheck="";
 
+function limpiar_dato($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
 //Si (llega datos)Entonces
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    print_r ($_POST);
+    
 
-    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"]) || !empty($_POST["street"]) || !empty($_POST["city"]) || !empty($_POST["state"]) || !empty($_POST["zip"]) || !empty($_POST["other"]) || !empty($_POST["news"]) || !empty($_POST["newscheck"])) {
     echo "<br><strong>name post hay datos</strong><br>";
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $street = $_POST["street"];
-    $city = $_POST["city"];
-    $state = $_POST["state"];
-    $zip = $_POST["zip"];
-    $other = $_POST["other"];
-    $news = $_POST["news"];
-    $newscheck = $_POST["newscheck"];
+    $name = limpiar_dato ($_POST["name"]);
+    echo "<strong> Nombre:</strong>" . $name. "<br>";
+    $email = limpiar_dato ($_POST["email"]);
+    echo "<strong> Email:</strong>" . $email. "<br>";
+    $phone = limpiar_dato ($_POST["phone"]);
+    echo "<strong> Telefono:</strong>" . $phone. "<br>";
+    $street = limpiar_dato ($_POST["street"]);
+    echo "<strong> Street:</strong>" . $Street. "<br>";
+    $city = limpiar_dato ($_POST["city"]);
+    echo "<strong> City:</strong>" . $citye. "<br>";
+    $state = limpiar_dato($_POST["state"]);
+    echo "<strong> State:</strong>" . $State. "<br>";
+    $zip = limpiar_dato ($_POST["zip"]);
+    echo "<strong> Zip:</strong>" . $Zip. "<br>";
+    $other = limpiar_dato($_POST["other"]);
+    echo "<strong> Other:</strong>" . $other. "<br>";
+    $news = limpiar_dato($_POST["news"]);
+    echo "<strong> News:</strong>" . $news. "<br>";
+    $newscheck = limpiar_dato($_POST["newscheck"]);
+    echo "<strong> Newscheck:</strong>" . $newscheck. "<br>";
     
-    function limpiar_dato($dato){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-    }
-
     function validar_name($name){
         if (!preg_match("/^[a-zA-Z-']*$/",$name)){
             return false;
@@ -35,6 +51,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             return true;
         }
     }
+
+    function validar_email($email){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+            }else{
+                return true;
+                }
+        }
+        
+        function validar_phone($phone){
+            if(preg_match('(/^[0-9]{10}+$/)',$phone)) {
+                echo"valid phone number";
+            } else {
+                echo "invalid phone number";
+            }
+        }
 
  
 
