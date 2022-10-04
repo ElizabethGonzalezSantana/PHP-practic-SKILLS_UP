798c81a3d71edaf8477a0d77e80fce156c78b431
Elizabeth González <elizabethpfaeskillsup@gmail.com>
2022-10-04 11:20:35 +0100

    Indexación

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -1,8 +1,8 @@
 <?php
-require"../requiere/config.php";
+require "../requiere/config.php";
 
 //define y inicializa las variables que se van a usar del formulario.
-$name = $email = $phone = $address = $city = $province = $zip = $other = $news = $newscheck="";
+$name = $email = $phone = $address = $city = $province = $zip = $other = $news = $newscheck = "";
 $name_err = $email_err = $phone_err = false;
 /**
  * Función para limpiar un dato procedente de un formulario.
@@ -17,7 +17,6 @@ function limpiar_dato($data)
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-
 }
 
 //nombre, email y nº de telefono.
@@ -28,124 +27,121 @@ function limpiar_dato($data)
  * @return boolean
  */
 
-    function validar_name($name)
-    {
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-            return false;
-            }else{
-                return true;
+function validar_name($name)
+{
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+        return false;
+    } else {
+        return true;
     }
 }
 
-    function validar_email($email)
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+function validar_email($email)
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return false;
-            }else{
-                return true;
-                }
-        }
-        
-        //TODO:documentar función.
-        /**
-         * Validar un número de telefono.
-         * @param $phone
-         * @return Boolean
-        */
+    } else {
+        return true;
+    }
+}
 
-        function validar_phone($phone){
-            if(!preg_match('(/^[0-9]+$/)',$phone)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    
+//TODO:documentar función.
+/**
+ * Validar un número de telefono.
+ * @param $phone
+ * @return Boolean
+ */
+
+function validar_phone($phone)
+{
+    if (!preg_match('(/^[0-9]+$/)', $phone)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 
 //Si (llega datos)Entonces
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    print_r ($_POST);
-    
-    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
-    echo "<br><strong>name post hay datos</strong><br>";
-    //Variables requeridas para enviar a BBDD:name,email y phone.
-    $name = limpiar_dato($_POST["name"]);
-    echo"$name <br>";
-    $email = limpiar_dato($_POST["email"]);
-    echo"$email <br>";
-    $phone = limpiar_dato($_POST["phone"]);
-    echo"$phone<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    print_r($_POST);
 
-    if (validar_name($name)){
-        
-    }else{
-        $name_err=true;
-    }
+    if (!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])) {
+        echo "<br><strong>name post hay datos</strong><br>";
+        //Variables requeridas para enviar a BBDD:name,email y phone.
+        $name = limpiar_dato($_POST["name"]);
+        echo "$name <br>";
+        $email = limpiar_dato($_POST["email"]);
+        echo "$email <br>";
+        $phone = limpiar_dato($_POST["phone"]);
+        echo "$phone<br>";
 
-    if (validar_email($email)){
-        
-    }else{
-        $email_err=true;
-    }
+        if (validar_name($name)) {
+        } else {
+            $name_err = true;
+        }
 
-    if (validar_phone($phone)){
-        
-    }else{
-        $phone_err=true;
-    }
+        if (validar_email($email)) {
+        } else {
+            $email_err = true;
+        }
 
-    if(validar_name($name) || validar_email($email) || validar_phone($phone)){
+        if (validar_phone($phone)) {
+        } else {
+            $phone_err = true;
+        }
 
-    }else{
-        if ($name_err==true){
-            echo "la validación de name ha fallado";
-        }elseif($email_err==true){
-            echo "La validación de email ha fallado";
-        }elseif($phone_err==true){
-            echo "La validación de phone ha fallado";
-        }   
-    }
+        if (validar_name($name) || validar_email($email) || validar_phone($phone)) {
+        } else {
+            if ($name_err == true) {
+                echo "la validación de name ha fallado";
+            } elseif ($email_err == true) {
+                echo "La validación de email ha fallado";
+            } elseif ($phone_err == true) {
+                echo "La validación de phone ha fallado";
+            }
+        }
 
 
-    if(isset($_POST["address"])){
-        $address = limpiar_dato ($_POST["address"]);
-    }else{
-        $address = NULL;
-    }
-    if(isset($_POST["city"])){
-        $city = limpiar_dato ($_POST["city"]);
-    }else{
-        $city = NULL;
-    }
-    if(isset($_POST["province"])){
-        $province = limpiar_dato ($_POST["province"]);
-    }else{
-        $province = NULL;
-    }
-    if(isset($_POST["zip"])){
-        $zip = limpiar_dato ($_POST["zip"]);
-    }else{
-        $zip = NULL;
-    }
-    if(isset($_POST["newscheck"])){
-        $newscheck = limpiar_dato ($_POST["newscheck"]);
-    }else{
-        $newscheck = NULL;
-    }
-    if(isset($_POST["news"])){
-        $news = limpiar_dato ($_POST["news"]);
-    }else{
-        $news = NULL;
-    }
-    if(isset($_POST["other"])){
-        $other = limpiar_dato ($_POST["other"]);
-    }else{
-        $other = NULL;
-    }
+        if (isset($_POST["address"])) {
+            $address = limpiar_dato($_POST["address"]);
+        } else {
+            $address = NULL;
+        }
+        if (isset($_POST["city"])) {
+            $city = limpiar_dato($_POST["city"]);
+        } else {
+            $city = NULL;
+        }
+        if (isset($_POST["province"])) {
+            $province = limpiar_dato($_POST["province"]);
+        } else {
+            $province = NULL;
+        }
+        if (isset($_POST["zip"])) {
+            $zip = limpiar_dato($_POST["zip"]);
+        } else {
+            $zip = NULL;
+        }
+        if (isset($_POST["newscheck"])) {
+            $newscheck = limpiar_dato($_POST["newscheck"]);
+        } else {
+            $newscheck = NULL;
+        }
+        if (isset($_POST["news"])) {
+            $news = limpiar_dato($_POST["news"]);
+        } else {
+            $news = NULL;
+        }
+        if (isset($_POST["other"])) {
+            $other = limpiar_dato($_POST["other"]);
+        } else {
+            $other = NULL;
+        }
 
 
-            //var_dump($newsletter);
+        //var_dump($newsletter);
         //echo "</br>";
         /*          $newsletter = filter_input(
                 INPUT_POST,
@@ -170,12 +166,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "<strong>Telefono:</strong> $phone <br>";
         echo "<strong>Email: </strong> $email <br>";
 
-        if (validar_name($name)){
+        if (validar_name($name)) {
             echo "validada";
         } else {
             echo "no valida";
         };
-
     }
 }
 
